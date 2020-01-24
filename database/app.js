afbedf85505d7570e55960b313e36311e5ffb5cd
Idan Madar <t882200@gmail.com>
2020-01-24 11:17:16 +0200

    code cleaning

--- database/app.js
+++ database/app.js
@@ -21,19 +21,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     dialect: "mysql"
 //   });
 
-// the wrong
-// const sequelize = new Sequelize('mysql:contacts.db');
-
-// var whitelist = ['http://localhost:3000/']
-// var corsOptions = {
-//     origin: function (origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1 || !origin) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error('Not allowed by CORS'))
-//       }
-//     }
-//   }
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
